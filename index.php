1af80a16bd467004546fac2fd28c8bcdf342bffa
kosta39 <kokson65@gmail.com>
2022-11-17 01:27:13 +0100

    opcije za rad sa proizvodima/zalihama na index.php strani

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="index.css" rel="stylesheet">
+    <title>ITEH PHP MYSQL AJAX</title>
+</head>
+
+<body>
+
+    <div class="div-index">
+
+        <h1 class="index-naslov">PHP MYQL AJAX - Domaći zadatak</h1>
+
+        <div class="div-index-izbor">
+            <button class="btn btn-primary btn-lg" id="novi-proizvod-dgm">Unesi novi proizvod</button>
+            <button class="btn btn-success btn-lg" id="azuriraj-zalihe-dgm">Ažuriraj zalihe</button>
+        </div>
+
+
+
+    </div>
+
+</body>
+
+</html>
